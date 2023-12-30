bac02adeb8925adf1d7c951965b9060407fec85a
Amirkhan <torexanovich.l@gmail.com>
2023-12-30 13:47:53 +0500

    completed

--- quiz-game/main.go
+++ quiz-game/main.go
@@ -6,41 +6,84 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
+	"math/rand"
 )
 
 func main() {
-	csvFilename := flag.String("csv", "problems.csv", "a csv file in the format of 'question,answer'")
+	csvFileName := flag.String("csv", "problems.csv", "CSV file with 'question,answer'")
+	timeLimit := flag.Int("time", 30, "time limit for the quiz in seconds")
+	shuffle := flag.Bool("shuffle", false, "shuffle quiz order")
 	flag.Parse()
 
-	file, err := os.Open(*csvFilename)
+	questions := readCSV(*csvFileName)
+	if *shuffle {
+		shuffleQuestions(questions)
+	}
+
+	fmt.Println("Press Enter to start the quiz...")
+	fmt.Scanln()
+
+	correct := runQuiz(questions, *timeLimit)
+
+	fmt.Printf("\nYou got %d out of %d questions correct.\n", correct, len(questions))
+}
+
+func readCSV(filename string) [][]string {
+	file, err := os.Open(filename)
 	if err != nil {
-		exit(fmt.Sprintf("Failed to open CSV file: %s", *csvFilename))
+		exit(fmt.Sprintf("Failed to open CSV file: %s", filename))
 	}
 	defer file.Close()
 
 	r := csv.NewReader(file)
 	lines, err := r.ReadAll()
 	if err != nil {
-		exit("Failed to parse the provided CSV file")
+		exit("Failed to parse the provided CSV file.")
 	}
 
-	var correct int
-	for _, line := range lines {
-		question := line[0]
-		answer := strings.TrimSpace(line[1])
+	return lines
+}
 
-		fmt.Printf("Question: %s\n", question)
+func shuffleQuestions(questions [][]string) {
+	rand.Seed(time.Now().UnixNano())
+	rand.Shuffle(len(questions), func(i, j int) {
+		questions[i], questions[j] = questions[j], questions[i]
+	})
+}
 
-		var userAnswer string
-		fmt.Print("Your answer: ")
-		fmt.Scanln(&userAnswer)
+func runQuiz(questions [][]string, timeLimit int) int {
+	timer := time.NewTimer(time.Duration(timeLimit) * time.Second)
+	correct := 0
 
-		if userAnswer == answer {
-			correct++
+questionLoop:
+	for _, q := range questions {
+		question, answer := q[0], strings.TrimSpace(q[1])
+
+		fmt.Printf("\nQuestion: %s\nYour answer: ", question)
+
+		select {
+		case <-timer.C:
+			fmt.Println("\nTime's up! Quiz completed.")
+			break questionLoop
+		case userAnswer := <-getUserAnswer():
+			if strings.TrimSpace(userAnswer) == answer {
+				correct++
+			}
 		}
 	}
 
-	fmt.Printf("\nYou got %d out of %d questions correct.\n", correct, len(lines))
+	return correct
+}
+
+func getUserAnswer() <-chan string {
+	answerCh := make(chan string)
+	go func() {
+		var userAnswer string
+		fmt.Scanln(&userAnswer)
+		answerCh <- userAnswer
+	}()
+	return answerCh
 }
 
 func exit(msg string) {
